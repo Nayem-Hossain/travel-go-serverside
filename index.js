a80867def091a02ebde480d1d7b4f7814f06a23a
Nayem-Hossain <nayem.hossain7298@gmail.com>
2021-11-02 21:16:51 +0600

    done

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
+app.use(express.json());
 
+//Database Connection URI
 const { MongoClient } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@travelgo-cluster.5yple.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@travelgo-cluster.5yple.mongodb.net/travelGoDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 console.log(uri);
+
+app.get("/", (req, res) => {
+  res.send("Running travelGo Server");
+});
+
+app.listen(port, () => {
+  console.log(`travelGo Server listening at http://localhost:${port}`);
+});
+
 /* client.connect((err) => {
   const collection = client.db(process.env.DB_NAME).collection("orders");
   // perform actions on the collection object
@@ -22,20 +35,29 @@ console.log(uri);
 async function run() {
   try {
     await client.connect();
-    console.log("nayem");
-    const database = client.db(process.env.DB_NAME);
+    console.log("connected to database");
+
+    const database = client.db("travelGoDatabase");
     const servicesCollection = database.collection("services");
     const ordersCollection = database.collection("orders");
 
     // GET ALL SERVICES API
     app.get("/services", async (req, res) => {
-      const cursor = servicesCollection.find({});
-      const services = await cursor.toArray();
+      const allService = servicesCollection.find({});
+      const services = await allService.toArray();
       res.send(services);
     });
 
-    // GET SINGLE SERVICES API
+    // POST ADD A NEW SERVICE API
+    app.post("/services", async (req, res) => {
+      const insertItem = req.body;
+      console.log("hitted the post services API", insertItem);
+      const result = await servicesCollection.insertOne(insertItem);
+      console.log(result);
+      res.json(result);
+    });
 
+    // GET SINGLE SERVICES API
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       console.log("getting specific service", id);
@@ -43,6 +65,7 @@ async function run() {
       const service = await servicesCollection.findOne(query);
       res.json(service);
     });
+
     // GET ORDER API
     app.get("/orders", async (req, res) => {
       const cursor = ordersCollection.find({});
@@ -53,10 +76,24 @@ async function run() {
     // POST ORDER API
     app.post("/orders", async (req, res) => {
       const order = req.body;
-      console.log("hit the post api", order);
-
+      console.log("hitted the post orders API", order);
       const result = await ordersCollection.insertOne(order);
       console.log(result);
+      res.json(result);
+    });
+
+    // UPDATE A DOCUMENT
+    app.put("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status: "approved",
+        },
+      };
+      const result = await ordersCollection.updateOne(filter, updateDoc);
+      console.log("updating user with id", result);
       res.json(result);
     });
 
@@ -68,18 +105,8 @@ async function run() {
       console.log("deleting user with id", result);
       res.json(result);
     });
-
-    // console.log(`A document was inserted with the _id: ${result.insertedId}`)
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-app.get("/", (req, res) => {
-  res.send("Welcome to travelGo Server");
-});
-
-app.listen(port, () => {
-  console.log(`travelGo Server listening at http://localhost:${port}`);
-});
